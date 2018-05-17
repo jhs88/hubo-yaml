d7facef0e037b4cccbc8e95a3951986a14317fa5
Joe Scherreik <jhs88@drexel.edu>
2018-05-17 10:21:43 -0400

    added support for degrees

--- multiGesture.py
+++ multiGesture.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
 import yaml
-
 from Maestor import maestor
+from math import pi
 
 robot = maestor()
 
-import yaml
-//Rename test.yaml to your YAML file
+# Rename test.yaml to your YAML file
 with open('test.yaml', 'r') as f:
     doc = yaml.load(f)
 
+if doc['units'] == 'deg':
+    convertFac = pi / 180
+else:
+    convertFac = 1
+
 for names in doc['run']:
     joints = ' '.join(doc[names])
-    positions = [doc[names][i] for i in doc[names]]
+    positions = [(doc[names][i] * convertFac) for i in doc[names]]
     strPositions = ' '.join([str(i) for i in positions])
     robot.setProperties(joints, ' '.join(len(doc[names]) * ['position']), strPositions)
--- oneGesture.py
+++ oneGesture.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import yaml
-
 from Maestor import maestor
+from math import pi
 
 robot = maestor()
 
-//Rename test.yaml to your YAML file
+# Rename test.yaml to your YAML file
 with open('test.yaml', 'r') as f:
     doc = yaml.load(f)
 
+if doc['units'] == 'deg':
+    convertFac = pi / 180
+else:
+    convertFac = 1
+
 joints = ' '.join(doc['gesture'])
-positions = [doc['gesture'][i] for i in doc['gesture']]
+positions = [(doc['gesture'][i] * convertFac) for i in doc['gesture']]
 strPositions = ' '.join([str(i) for i in positions])
 
 robot.setProperties(joints, ' '.join(len(doc['gesture']) * ['position']), strPositions)
